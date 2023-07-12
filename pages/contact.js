da25c6be1956450414f13dbe1175c1d4d636905a
Mikolaj Kos <mkk3@onet.pl>
2023-07-12 20:22:45 +0200

    add dummy working API and establish connection to database

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactForm from "../components/contact/contact-form";
+
+function ContactPage() {
+  return (
+    <React.Fragment>
+      <ContactForm />
+    </React.Fragment>
+  );
+}
+
+export default ContactPage;
