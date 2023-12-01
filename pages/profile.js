3c05631d1f8d428bc49fc125bad4d16b4ac8ed1d
Mikolaj Kos <mkk3@onet.pl>
2023-12-01 23:11:03 +0100

    add final changes

--- pages/profile.js
+++ pages/profile.js
@@ -7,7 +7,7 @@ import { authOptions } from "./api/auth/[...nextauth]";
 import Profile from "../components/profile/profile";
 import { connectDatabase } from "../helpers/db-util";
 import mongoose from "mongoose";
-import Lake from "../models/Lake.js";
+import Lake from "../models/Lake";
 import User from "../models/User";
 
 function ProfilePage(props) {
